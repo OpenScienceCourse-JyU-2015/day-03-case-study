c7933c481063a197b6393a34d0680ce778129ea0
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2016-11-11 20:53:05 +0200

    Start work on Python script to convert fasta to table

---
+++ scripts/fasta2table.py
@@ -0,0 +1,32 @@
+### * Description
+
+# Convert a fasta file containing protein sequences into a tab-separated table
+
+# Input: name of a fasta file
+# Output: write the output table to stdout
+
+# Usage: python fasta2table.py myInputFile > myOutputFile
+
+# Depends on the Biopython package to read the fasta files
+
+### * Import
+
+import sys # To get command line arguments
+from Bio import SeqIO
+
+### * Functions
+
+### * Run
+
+# Get the input file name
+inputFile = sys.argv[1]
+
+# Load all the sequences from the fasta file
+with open(inputFile, "r") as fi:
+    fasta = SeqIO.parse(fi, "fasta")
+    sequences = list(fasta)
+
+# Print sequences
+for seq in sequences:
+    print(seq.description)
+    print(seq.seq)
