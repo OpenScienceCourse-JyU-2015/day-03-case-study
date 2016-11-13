54216ea3b36800c8e4c2be92c071087fd0b7b6ef
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2016-11-13 17:39:35 +0200

    Randomize rows in Fishbase tables

--- scripts/splitFishbase.py
+++ scripts/splitFishbase.py
@@ -5,6 +5,7 @@
 ### * Import
 
 import os
+import random
 import pandas as pd
 
 ### * Parameters
@@ -24,7 +25,13 @@ temp = fb.loc[:, ["species", "temperature_min", "temperature_max", "climate", "e
 environment = fb.loc[:, ["species", "climate", "environment", "depth_min", "depth_max"]]
 extra_data = fb.loc[:, ["species", "metabolism_data", "speed_data", "gill_area_data"]]
 
+# Shuffle the rows
+# http://stackoverflow.com/questions/1022141/best-way-to-randomize-a-list-of-strings-in-python
+temp = temp.iloc[random.sample(range(len(fb)), len(fb))]
+environment = environment.iloc[random.sample(range(len(fb)), len(fb))]
+extra_data = extra_data.iloc[random.sample(range(len(fb)), len(fb))]
+
 # Save the three new tables
-temp.to_csv(TEMPERATURE_TSV, sep = "\t")
-environment.to_csv(ENVIRONMENT_TSV, sep = "\t")
-extra_data.to_csv(EXTRA_DATA_TSV, sep = "\t")
+temp.to_csv(TEMPERATURE_TSV, sep = "\t", index = False)
+environment.to_csv(ENVIRONMENT_TSV, sep = "\t", index = False)
+extra_data.to_csv(EXTRA_DATA_TSV, sep = "\t", index = False)
